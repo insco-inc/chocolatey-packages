20a0154ca9d26a7643e27defac41ed24beb113a9
kjxbyz <kjxbyz888@outlook.com>
2025-05-03 20:35:28 +0800

    fix: update manifests

--- automatic/picguard/update.ps1
+++ automatic/picguard/update.ps1
@@ -8,10 +8,10 @@ $headers = @{
 }
 
 function global:au_GetLatest {
-    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/picguard/picguard/releases/latest" -Headers $headers
+    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/PicGuard/PicGuard/releases/latest" -Headers $headers
     $LatestTag = $LatestRelease.tag_name
     $LatestVersion = $LatestTag.Replace('v', '').Replace('+', '.')
-    $LatestURL64 = ($LatestRelease.assets | Where-Object {$_.name.StartsWith("picguard-") -and $_.name.EndsWith("-windows-setup-x64.exe")}).browser_download_url
+    $LatestURL64 = ($LatestRelease.assets | Where-Object {$_.name.StartsWith("picguard-") -and -not $_.name.StartsWith("picguard-pro-") -and $_.name.EndsWith("-windows-setup-x64.exe")}).browser_download_url
 
     if (!$LatestURL64) {
         throw "64bit URL is missing!"
@@ -23,8 +23,8 @@ function global:au_GetLatest {
         URL64        = $LatestURL64
         Tag          = $LatestTag
         Version      = $LatestVersion
-        ReleaseNotes = $LatestRelease.body
-        IconUrl      = "https://raw.githubusercontent.com/picguard/picguard/$($LatestTag)/logo/logo.png"
+        ReleaseNotes = $LatestRelease.html_url
+        IconUrl      = "https://raw.githubusercontent.com/PicGuard/PicGuard/$($LatestTag)/logo/logo.png"
     }
 }
 
@@ -37,8 +37,9 @@ function global:au_SearchReplace {
         }
 
         "picguard.nuspec" = @{
-            "(\<iconUrl\>).*?(\</iconUrl\>)"           = "`$1$($Latest.IconUrl)`$2"
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1`n  $($Latest.ReleaseNotes)`n`$2"
+            "(\<version\>).*?(\</version\>)"           = "`${1}$($Latest.Version)`$2"
+            "(\<iconUrl\>).*?(\</iconUrl\>)"           = "`${1}$($Latest.IconUrl)`$2"
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
         }
     }
 }
