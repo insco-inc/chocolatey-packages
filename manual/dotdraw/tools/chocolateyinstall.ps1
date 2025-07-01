4c651ece9e427064ff7fff0173d043f3ce23908d
kjxbyz <kjxbyz888@outlook.com>
2025-07-02 07:01:36 +0800

    fix: update dotdraw

--- manual/dotdraw/tools/chocolateyinstall.ps1
+++ manual/dotdraw/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/DotDraw/dotdraw/releases/download/v0.0.1%2B5.pre/dotdraw-0.0.1.5.pre-windows-x64.exe'
+$url64      = 'https://github.com/DotDraw/dotdraw/releases/download/v0.0.1%2B5.pre/dotdraw-0.0.1.5.pre-windows-setup-x64.exe'
 
 $arch = Get-OSArchitectureWidth -Compare 64
 
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'dotdraw*'
 
-  checksum64    = '407e91ba43df76283a53cdd5687906252772246a7a5e0a4d2f7e8297aa6ca468'
+  checksum64    = '8c0587089dfdfeb653f229e26996c9fd456597b1e1c047ca14f70171eb000bc5'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
