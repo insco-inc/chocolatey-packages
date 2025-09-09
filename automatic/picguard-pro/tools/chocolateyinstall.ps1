4dddea1cc04991af765b7daa50143a7c7ed2bb1c
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2025-09-09 11:21:19 +0000

    AU: 2 updated - picguard picguard-pro [skip ci] https://gist.github.com/kjxbyz/6e62e5cab8fb28487b265620bd85755d/249f5b286a4d1598a7cbd3e8c7875f4d934fcc3e

--- automatic/picguard-pro/tools/chocolateyinstall.ps1
+++ automatic/picguard-pro/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/picguard/picguard/releases/download/v4.0.2/picguard-pro-4.0.2-windows-setup-x64.exe'
+$url64      = 'https://github.com/picguard/picguard/releases/download/v5.0.0/picguard-pro-5.0.0-windows-setup-x64.exe'
 
 $arch = Get-OSArchitectureWidth -Compare 64
 
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'picguard-pro*'
 
-  checksum64    = 'c74411802f9ba96577c92e5c3207e104764af681afe61c72eea719a087193123'
+  checksum64    = 'e76bc78e2d184068d126806dfb9ea4ff89db6b7aaadc3bad0ab831c686d4cd03'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
